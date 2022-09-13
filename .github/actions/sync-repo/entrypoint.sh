33bfee636a1b31bd2bde6abc07c9872dfe2a71d3
Konstantin Golovin <konstantingolovin@MacBook-Pro-Konstantin.local>
2022-09-13 12:05:09 +0300

    debug

--- .github/actions/sync-repo/entrypoint.sh
+++ .github/actions/sync-repo/entrypoint.sh
@@ -5,40 +5,57 @@ set -x
 
 if [ -z "$INPUT_SOURCE_FOLDER" ]
 then
-  INPUT_SOURCE_FOLDER=dist
+  echo "Source folder must be defined"
+  return -1
 fi
 
-if [ -z "$INPUT_DESTINATION_BRANCH" ]
+if [ $INPUT_DESTINATION_HEAD_BRANCH == "main" ] || [ $INPUT_DESTINATION_HEAD_BRANCH == "master"]
 then
-  INPUT_DESTINATION_BRANCH=master
+  echo "Destination head branch cannot be 'main' nor 'master'"
+  return -1
 fi
 
-if [ -z "$INPUT_COMMIT_MSG" ]
+if [ -z "$INPUT_PULL_REQUEST_REVIEWERS" ]
 then
-  INPUT_COMMIT_MSG="Update $INPUT_DESTINATION_FOLDER."
+  PULL_REQUEST_REVIEWERS=$INPUT_PULL_REQUEST_REVIEWERS
+else
+  PULL_REQUEST_REVIEWERS='-r '$INPUT_PULL_REQUEST_REVIEWERS
 fi
 
 CLONE_DIR=$(mktemp -d)
 
-echo "Cloning destination git repository"
+echo "Setting git variables"
+export GITHUB_TOKEN=$API_TOKEN_GITHUB
 git config --global user.email "$INPUT_USER_EMAIL"
 git config --global user.name "$INPUT_USER_NAME"
-git clone --single-branch --branch $INPUT_DESTINATION_BRANCH "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
-echo "Removing old destination files"
-rm -rf $CLONE_DIR/$INPUT_DESTINATION_FOLDER/*
+echo "Cloning destination git repository"
+git clone "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
 
 echo "Copying contents to git repo"
-cp -rf $INPUT_SOURCE_FOLDER/. $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
+echo $CLONE_DIR;
+echo $INPUT_DESTINATION_FOLDER;
+rm -rf "$CLONE_DIR/wp-content/themes/gg/"
+mkdir "$CLONE_DIR/wp-content/themes/gg"
+cp -a "$INPUT_SOURCE_FOLDER." "$CLONE_DIR/$INPUT_DESTINATION_FOLDER"
+touch "$CLONE_DIR/wp-content/themes/gg/update_date.txt"
+date > "$CLONE_DIR/wp-content/themes/gg/update_date.txt"
 cd "$CLONE_DIR"
+git checkout -b "$INPUT_DESTINATION_HEAD_BRANCH"
 
 echo "Adding git commit"
 git add .
 if git status | grep -q "Changes to be committed"
 then
-  git commit --message "$INPUT_COMMIT_MSG"
+  git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
   echo "Pushing git commit"
-  git push origin $INPUT_DESTINATION_BRANCH
+  git push -u origin HEAD:$INPUT_DESTINATION_HEAD_BRANCH
+  echo "Creating a pull request"
+  gh pr create -t $INPUT_DESTINATION_HEAD_BRANCH \
+               -b $INPUT_DESTINATION_HEAD_BRANCH \
+               -B $INPUT_DESTINATION_BASE_BRANCH \
+               -H $INPUT_DESTINATION_HEAD_BRANCH \
+                  $PULL_REQUEST_REVIEWERS
 else
   echo "No changes detected"
 fi
